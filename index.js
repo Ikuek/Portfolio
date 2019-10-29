01b71b250593f8e2ea0fe0ed80c6451beddc8bc9
Ikuek <55440433+Ikuek@users.noreply.github.com>
2019-10-29 23:07:59 +0900

    Update index.js

--- index.js
+++ index.js
@@ -1,17 +1,9 @@
-
-//メニュークリックで該当セクションまでスクロール
 $(function(){
-    // #で始まるアンカーをクリックした時の処理
     $('a[href^=#]').click(function() {
-       // スクロールの速度
-       var speed = 400; // ミリ秒
-       // アンカーの値を取得
+       var speed = 400;
        var href= $(this).attr("href");
-       // 移動先を取得
        var target = $(href == "#" || href == "" ? 'html' : href);
-       // 移動先を数値で取得
        var position = target.offset().top;
-       // スムーススクロール
        $('body,html').animate({scrollTop:position}, speed, 'swing');
        return false;
     });
